4087140dd5b0139092f6cdd8426271fe574e3de7
Sahilvats2003 <ss0447096@gmail.com>
2024-03-31 22:06:54 +0530

    GptMovieSearch

--- src/components/GptMovieSuggestions.js
+++ src/components/GptMovieSuggestions.js
@@ -1,9 +1,26 @@
-import React from 'react'
+import React from "react";
+import { useSelector } from "react-redux";
+import MovieList from "./MovieList";
 
 const GptMovieSuggestions = () => {
-  return (
-    <div>GptMovieSuggestions</div>
-  )
-}
+  const { moviesResults, movieNames } = useSelector((store) => store.gpt);
 
-export default GptMovieSuggestions
+  if (!movieNames) return null;
+  if(!moviesResults) return null;
+
+  return (
+    <div className="p-4 m-4 bg-black text-white bg-opacity-70">
+      <div>
+        {movieNames.map((movieName,index) => (
+          <MovieList
+            key={movieName}
+            title={movieName}
+            movies={moviesResults[index]}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default GptMovieSuggestions;
